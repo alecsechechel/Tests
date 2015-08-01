aa634dbdae65943e0e007141ac38c4d17aa89a5f
Alex <alex@MacBook-Air-Alex.local>
2015-08-01 18:23:09 +0300

    good

--- XcodeServer/XcodeServerTests/XcodeServerTests.swift
+++ XcodeServer/XcodeServerTests/XcodeServerTests.swift
@@ -50,7 +50,7 @@ class XcodeServerTests: XCTestCase {
         XCTAssertEqual(num1 * num1, math.sqr(num1), "Error sqr")
         
         num1 = -2
-        XCTAssertEqual(1, math.sqr(num1), "Error sqr")
+        XCTAssertEqual(0, math.sqr(num1), "Error sqr")
     }
     
 }
